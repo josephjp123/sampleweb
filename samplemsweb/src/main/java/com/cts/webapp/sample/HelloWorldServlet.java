07b7f55eb53e112bbfd52c1aa0caaf6b4b1f4b97
josephjp123 <josephjp333@gmail.com>
2015-10-25 15:59:10 +0530

    Update HelloWorldServlet.java

--- samplemsweb/src/main/java/com/cts/webapp/sample/HelloWorldServlet.java
+++ samplemsweb/src/main/java/com/cts/webapp/sample/HelloWorldServlet.java
@@ -1,27 +1,27 @@
 package com.cts.webapp.sample;
 
-//import java.io.IOException;
-//import java.io.PrintWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-//import javax.servlet.ServletException;
-//import javax.servlet.http.HttpServlet;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-//public class HelloWorldServlet extends HttpServlet {
+public class HelloWorldServlet extends HttpServlet {
 	
-//	private static final long serialVersionUID = 1031422249396784970L;
-//
-//	public void doGet(HttpServletRequest req, HttpServletResponse resp)
-//			throws ServletException, IOException {
-//		
-//		resp.setContentType("text/html");
-//		
-//		PrintWriter out = resp.getWriter();
-//		out.print("<h1> code updated Training docker .."+new java.util.Date()+"</h1>");
-//		out.flush();
-//		out.close();	}
-//}
+	private static final long serialVersionUID = 1031422249396784970L;
+
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		resp.setContentType("text/html");
+		
+		PrintWriter out = resp.getWriter();
+		out.print("<h1> code updated Training docker .."+new java.util.Date()+"</h1>");
+		out.flush();
+		out.close();	}
+}
 
 
 
