2c6029a3218bfd381a3b7843ff2038bcd43d412d
josephjp123 <josephjp333@gmail.com>
2015-10-25 15:48:28 +0530

    Update HelloWorldServlet.java

--- samplemsweb/src/main/java/com/cts/webapp/sample/HelloWorldServlet.java
+++ samplemsweb/src/main/java/com/cts/webapp/sample/HelloWorldServlet.java
@@ -23,33 +23,7 @@ package com.cts.webapp.sample;
 //		out.close();	}
 //}
 
-import java.io.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
 
-public class HelloWorldServlet extends HttpServlet
-{
-        
-        public void doPost(HttpServletRequest request,HttpServletResponse response) throws IOException,ServletException
-        {
-                display();
-                PrintWriter out = response.getWriter();
-                //out.println("hello");
-                String n1 = request.getParameter("no1");
-                String n2 = request.getParameter("no2");
-                String opt = request.getParameter("opt");                
-                if(opt.equals("p"))
-                        out.println(Integer.parseInt(n1) + Integer.parseInt(n2));
-                else if(opt.equals("m"))
-                       out.println(Integer.parseInt(n1) - Integer.parseInt(n2));
-        }
-        
-        public void doGet(HttpServletRequest request,HttpServletResponse response) throws IOException,ServletException
-        {
-                doPost(request,response);
-        }
-        
-}
 
 
 
