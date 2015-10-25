3c3942bf928786b95c4af0e1e1ef7cc2748de8c6
josephjp123 <josephjp333@gmail.com>
2015-10-25 13:26:13 +0530

    Update HelloWorldServlet.java

--- samplemsweb/src/main/java/com/cts/webapp/sample/HelloWorldServlet.java
+++ samplemsweb/src/main/java/com/cts/webapp/sample/HelloWorldServlet.java
@@ -1,12 +1,12 @@
-package com.cts.webapp.sample;
+//package com.cts.webapp.sample;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+//import java.io.IOException;
+//import java.io.PrintWriter;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+//import javax.servlet.ServletException;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
 
 //public class HelloWorldServlet extends HttpServlet {
 	
@@ -23,36 +23,72 @@ import javax.servlet.http.HttpServletResponse;
 //		out.close();	}
 //}
 
-
-public class HelloWorldServlet extends HttpServlet
-{
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-    {
-        try
-        {
-        	response.setContentType("text/html");
-        PrintWriter out= response.getWriter();
-        int a1= Integer.parseInt(request.getParameter("n1"));
-        int a2= Integer.parseInt(request.getParameter("n2"));
-        if(request.getParameter("r1")!=null)
-        {
-            out.println("<h1>Addition</h1>"+(a1+a2));
-        }
-        if(request.getParameter("r2")!=null)
-        {
-            out.println("<h1>Substraction</h1>"+(a1-a2));
-        }
-        if(request.getParameter("r3")!=null)
-        {
-            out.println("<h1>Multiplication</h1>"+(a1*a2));
-        }if(request.getParameter("r1")!=null)
-        {
-            out.println("<h1>Division</h1>"+(a1/a2));
-        }
-        }
-        catch(Exception e)
-        {
-
-        }
-    }
+package de.duckout.servlet;
+import java.io.IOException;
+import java.util.Map;
+ 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+/**
+* Servlet implementation class Calculator
+*/
+@WebServlet("/Calculator")
+public class Calculator extends HttpServlet {
+private static final long serialVersionUID = 1L;
+double result;
+ 
+public Calculator() {
+super();
+// TODO Auto-generated constructor stub
+}
+ 
+protected void doGet(HttpServletRequest request,
+HttpServletResponse response) throws ServletException, IOException {
+// TODO Auto-generated method stub
+ 
+}
+ 
+protected void doPost(HttpServletRequest request,
+HttpServletResponse response) throws ServletException, IOException {
+ 
+String valueString = request.getParameter("value");
+String resultString = request.getParameter("result");
+ 
+double result = 0;
+if (!resultString.isEmpty()) {
+result = Double.parseDouble(resultString);
+}
+ 
+if (!valueString.isEmpty()) {
+ 
+double value = Double.parseDouble(valueString);
+ 
+Map parameters = request.getParameterMap();
+if (parameters.containsKey("plus")) {
+result += value;
+} else if (parameters.containsKey("minus")) {
+result -= value;
+} else if (parameters.containsKey("divide")) {
+result /= value;
+} else if (parameters.containsKey("multiply")) {
+result *= value;
+}
+ 
+request.setAttribute("value", result);
+ 
+RequestDispatcher view = request.getRequestDispatcher("/Calc.jsp");
+view.forward(request, response);
+ 
+}else {
+ 
+RequestDispatcher view = request.getRequestDispatcher("/Calc.jsp");
+view.forward(request, response);
+ 
+}
+}
 }
