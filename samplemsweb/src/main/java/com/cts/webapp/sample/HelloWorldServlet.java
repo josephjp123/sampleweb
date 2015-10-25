519bba1e22e786bdc86910d36e7069d8baeda1f7
josephjp123 <josephjp333@gmail.com>
2015-10-25 13:09:49 +0530

    Update HelloWorldServlet.java

--- samplemsweb/src/main/java/com/cts/webapp/sample/HelloWorldServlet.java
+++ samplemsweb/src/main/java/com/cts/webapp/sample/HelloWorldServlet.java
@@ -30,6 +30,41 @@ public class HelloWorldServlet extends HttpServlet
     {
         try
         {
+        	
+        	<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Calculator</title>
+</head>
+<body>
+<h1 style="text_align=center">Calculator</h1>
+<form method="get" action="/Servlet">
+<label>first number:</label>
+<input type="text" name="n1" />
+<br />
+<label>Second number : </label>
+<input type="text" name="n2" />
+<br />
+ <div>
+<label>
+<input type="radio" name="r1" value="add" />addition
+<br />
+</label>
+<input type="radio" name="r2" value="sub" />subtraction 
+<br />
+<input type="radio" name="r3" value="mul" />multiplication
+<br />
+<input type="radio" name="r4" value="div" />division
+<br /> 
+</div>
+<input type="button" value="submit" />
+</form>
+</body>
+</html>
+        	
+        	
+        	
         response.setContentType("text/html");
         PrintWriter out= response.getWriter();
         int a1= Integer.parseInt(request.getParameter("n1"));
