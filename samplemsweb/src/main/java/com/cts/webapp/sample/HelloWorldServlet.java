53759c88f1f9d2e4a39c635ed7a7c90f892c8101
josephjp123 <josephjp333@gmail.com>
2015-10-25 15:55:22 +0530

    Update HelloWorldServlet.java

--- samplemsweb/src/main/java/com/cts/webapp/sample/HelloWorldServlet.java
+++ samplemsweb/src/main/java/com/cts/webapp/sample/HelloWorldServlet.java
@@ -1,27 +1,5 @@
 package com.cts.webapp.sample;
 
-<html>
-        <body>
-                <form method="get" action="./CalculateServlet">
-                        Number 1 : <input type="text" name="no1"> <br>
-                        Number 2 : <input type="text" name="no2"> <br> 
-                        Operator : 
-                        
-                        <select name="opt">
-                                <option value="p"> + </option>
-                                <option value="m"> - </option>
-                                <option value="mul"> * </option>
-                                <option value="d"> / </option>
-                                                                                                                                
-                        </select>
-                                
-                        <br>
-                        <input type="submit" value="Go">
-                </form>
-        </body>
-</html>
-
-
 //import java.io.IOException;
 //import java.io.PrintWriter;
 
