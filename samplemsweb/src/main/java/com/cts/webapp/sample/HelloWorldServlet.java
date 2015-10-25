4e9b0d502b1adee7cce51559c7ef404c0922399e
josephjp123 <josephjp333@gmail.com>
2015-10-25 13:13:03 +0530

    Update HelloWorldServlet.java

--- samplemsweb/src/main/java/com/cts/webapp/sample/HelloWorldServlet.java
+++ samplemsweb/src/main/java/com/cts/webapp/sample/HelloWorldServlet.java
@@ -30,8 +30,11 @@ public class HelloWorldServlet extends HttpServlet
     {
         try
         {
+        	response.setContentType("text/html");
+        PrintWriter out= response.getWriter();
         	
-        	<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+        	
+       out.print("<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
@@ -61,12 +64,11 @@ public class HelloWorldServlet extends HttpServlet
 <input type="button" value="submit" />
 </form>
 </body>
-</html>
+</html>");
         	
         	
         	
-        response.setContentType("text/html");
-        PrintWriter out= response.getWriter();
+        
         int a1= Integer.parseInt(request.getParameter("n1"));
         int a2= Integer.parseInt(request.getParameter("n2"));
         if(request.getParameter("r1")!=null)
